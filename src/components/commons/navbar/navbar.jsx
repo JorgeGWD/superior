1576505fbb21793fb04d9a388dc9df2cbb67ab1e
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-18 12:35:42 -0500

    Add menu elements and conigure css

--- src/components/commons/navbar/navbar.jsx
+++ src/components/commons/navbar/navbar.jsx
@@ -1,11 +1,18 @@
 import './navbar.css'
 import LogoSuperior from '../../../assets/images/logo-superior.svg'
+import Button from '../button/button'
 
 const Navbar = () => {
     return (
         <nav>
             <div className="navbar__content">
-                <img src={LogoSuperior} alt="Logo Superior" />
+                <a href='#home'><img src={LogoSuperior} alt="Logo Superior" /></a>
+                <div className="navbar__content-menu">
+                    <a href="#services">Servicios</a>
+                    <a href="#about">Sobre Nosotros</a>
+                    <a href="#about">Nuestras Plantas</a>
+                    <Button text={'Contacto'} />
+                </div>
             </div>
         </nav>
     )
