5c92c5abf941c6db44e6157ac024ca8cb7786d10
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-19 14:00:25 -0500

    Add and adjust footer component content

--- src/components/commons/footer/footer.jsx
+++ src/components/commons/footer/footer.jsx
@@ -1,10 +1,53 @@
+import LogoSuperior from '../../../assets/images/logo-superior.svg'
+import Facebook from '../../../assets/images/social-facebook.svg'
+import Instagram from '../../../assets/images/social-instagram.svg'
+import Youtube from '../../../assets/images/social-youtube.svg'
+import LinkedIn from '../../../assets/images/social-linkedin.svg'
+import Pin from '../../../assets/images/icon-pin.svg'
+import Email from '../../../assets/images/icon-email.svg'
+import Phone from '../../../assets/images/icon-phone.svg'
 import './footer.css'
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer__content">
-                <p>© 2023 Your Company. All rights reserved.</p>
+                <img src={LogoSuperior} alt="Logo Superior" className='logo-superior' />
+                <div className="footer__content-socials">
+                    <h2>Siguenos:</h2>
+                    <a href="https://www.linkedin.com/" target="_blank"><img src={LinkedIn} alt="LinkedIn" /></a>
+                    <a href="https://www.youtube.com/" target="_blank"><img src={Youtube} alt="Youtube" /></a>
+                    <a href="https://www.facebook.com/" target="_blank"><img src={Facebook} alt="Facebook" /></a>
+                    <a href="https://www.instagram.com/" target="_blank"><img src={Instagram} alt="Instagram" /></a>
+                </div>
+                <div className="footer__content-locations">
+                    <h2>Localización:</h2>
+                    <div className="footer__content-item">
+                        <img src={Pin} alt="Pin" />
+                        <div className="footer__content-location-office">
+                            <h3>Oficinas administrativas:</h3>
+                            <p>Calle 110 43-448 AV Circunvalar Barranquilla.</p>
+                        </div>
+                    </div>
+                    <div className="footer__content-item">
+                        <img src={Pin} alt="Pin" />
+                        <div className="footer__content-location-floor">
+                            <h3>Plantas:</h3>
+                            <p>Kilometro 7 vía Puerto Colombia-Atlántico.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer__content-contac">
+                    <h2>Contactos:</h2>
+                    <div className="footer__content-item">
+                        <img src={Email} alt="Email" />
+                        <p>info@grupominerosuperior.com</p>
+                    </div>
+                    <div className="footer__content-item">
+                        <img src={Phone} alt="Phone" />
+                        <p>3159272862</p>
+                    </div>
+                </div>
             </div>
         </footer>
     )
