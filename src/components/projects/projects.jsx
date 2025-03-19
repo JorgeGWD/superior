ff2efaeb31439decd0878155b0b9e9732da02fee
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-19 12:50:41 -0500

    Create projects component

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,13 @@
+import Title from '../commons/title/title'
+import './projects.css'
+
+const Projects = () => {
+    return (
+        <div className='projects' id='projects'>
+            <Title className={'title__content-line'} text={'Nuestros Proyectos'} />
+            <p>Add your project components or content here</p>
+        </div>
+    )
+}
+
+export default Projects
