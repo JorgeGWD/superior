bfb526a1e7562f8119575b39597d92435ae752f2
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-21 14:03:14 -0500

    Adjust imgframe in projects component

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,11 +1,19 @@
+import ImgFrame from '../commons/imgframe/imgframe'
 import Title from '../commons/title/title'
 import './projects.css'
+import Project1 from '../../assets/images/equipos.jpg'
+import Project2 from '../../assets/images/plantas.jpg'
+import Project3 from '../../assets/images/proyectos.jpg'
 
 const Projects = () => {
     return (
         <div className='projects' id='projects'>
             <Title className={'title__content-line'} text={'Nuestros Proyectos'} />
-            <p>Add your project components or content here</p>
+            <div className="projects__content">
+                <ImgFrame src={Project1} alt={'Equipos'} text={'Ver más'} />
+                <ImgFrame src={Project2} alt={'Plantas'} text={'Ver más'} />
+                <ImgFrame src={Project3} alt={'Proyectos'} text={'Ver más'} />
+            </div>
         </div>
     )
 }
