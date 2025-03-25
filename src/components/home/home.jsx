feaf2f37aaf7da1e4adc10dc5b2a0517d092466a
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-25 13:25:02 -0500

    Set video and configure as background in home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,20 @@
 import Button from '../commons/button/button'
-import  './home.css'
+import './home.css'
+import Video from '../../assets/videos/superior-background-video.mp4'
+import Poster from '../../assets/images/planta-de-trituracion.jpg'
 
 const Home = () => {
     return (
         <section className='home' id='home'>
+            <video className='home__video' poster={Poster} autoPlay loop muted>
+                <source src={Video} type='video/mp4' />
+                Tu navegador no soporta la etiqueta de video.
+            </video>
+
             <div className="container home__content">
                 <h1>Grupo Minero Superior</h1>
                 <p>Ofrecemos soluciones integrales para la ejecución de grandes proyectos de ingeniería civil del sector industrial e inmobiliario.</p>
-                <Button className={'button__secondary'} text={'Más Sobre Nosotros'} />
+                <Button className={'button__secondary'} text={'Contáctanos'} />
             </div>
         </section>
     )
