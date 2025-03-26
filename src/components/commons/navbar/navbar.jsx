091d313d1c7abc30bda61fb2d6f69198f1ac5d16
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-26 13:53:10 -0500

    Add new images and adjust responsive menu

--- src/components/commons/navbar/navbar.jsx
+++ src/components/commons/navbar/navbar.jsx
@@ -1,5 +1,7 @@
 import './navbar.css'
 import LogoSuperior from '../../../assets/images/logo-superior.svg'
+import IconMenu from '../../../assets/images/icon-menu.svg'
+import IconClose from '../../../assets/images/icon-close.svg'
 import Button from '../button/button'
 
 const Navbar = () => {
@@ -7,6 +9,11 @@ const Navbar = () => {
         <nav>
             <div className="container navbar__content">
                 <a href='#home'><img src={LogoSuperior} alt="Logo Superior" /></a>
+                <input type="checkbox" id="check" />
+                <label htmlFor="check" className="navbar__content-checkbtn">
+                    <img src={IconMenu} alt="Menu" className='open-menu' />
+                    <img src={IconClose} alt="Close" className='close-menu' />
+                </label>
                 <div className="navbar__content-menu">
                     <a href="#services">Servicios</a>
                     <a href="#about">Sobre Nosotros</a>
