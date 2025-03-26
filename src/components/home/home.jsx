fda1cca16e36c6a9ae6a569591f7be77e7c5abbd
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-26 15:55:07 -0500

    Adjust primary title

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,6 +2,7 @@ import Button from '../commons/button/button'
 import './home.css'
 import Video from '../../assets/videos/superior-background-video.mp4'
 import Poster from '../../assets/images/planta-de-trituracion.jpg'
+import Title from '../commons/title/title'
 
 const Home = () => {
     return (
@@ -12,11 +13,11 @@ const Home = () => {
             </video>
 
             <div className="container home__content">
-                <h1>Grupo Minero Superior</h1>
+                <Title className={'title__primary'} text={'Grupo Minero Superior'} />
                 <p>Ofrecemos soluciones integrales para la ejecución de grandes proyectos de ingeniería civil del sector industrial e inmobiliario.</p>
                 <Button className={'button__secondary'} text={'Contáctanos'} />
                 <div className="home__content-bottom">
-                    <h2>TU ALIADO ESTRATÉGICO</h2>
+                    <Title className={'title__content-uppercase'} text={'Tu aliado estratégico'} />
                 </div>
             </div>
         </section>
