61e724cf9d20a915e8b23885376a79789c4fa8c8
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-28 14:17:36 -0500

    Add new button param

--- src/components/commons/button/button.jsx
+++ src/components/commons/button/button.jsx
@@ -1,8 +1,8 @@
 import './button.css'
 
-const Button = ({ text, className }) => {
+const Button = ({ text, className, type }) => {
     return (
-        <button className={`button ${className}`}>
+        <button className={`button ${className}`} type={type}>
             {text}
         </button>
     )
