bebafdf7ff81b953fc2fbcdb7267be9b4014194e
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-28 15:40:27 -0500

    Add navigation link to contacto

--- src/components/top-home/top-home.jsx
+++ src/components/top-home/top-home.jsx
@@ -2,6 +2,7 @@ import Button from '../commons/button/button'
 import Video from '../../assets/videos/superior-background-video.mp4'
 import Poster from '../../assets/images/planta-de-trituracion.jpg'
 import Title from '../commons/title/title'
+import { Link } from 'react-router'
 import './top-home.css'
 
 const TopHome = () => {
@@ -15,7 +16,7 @@ const TopHome = () => {
             <div className="container home__content">
                 <Title className={'title__primary'} text={'Grupo Minero Superior'} />
                 <p>Ofrecemos soluciones integrales para la ejecución de grandes proyectos de ingeniería civil del sector industrial e inmobiliario.</p>
-                <Button className={'button__secondary'} text={'Contáctanos'} />
+                <Link to={'/contacto'}><Button className={'button__secondary'} text={'Contáctanos'} /></Link>
                 <div className="home__content-bottom">
                     <Title className={'title__content-uppercase'} text={'Tu aliado estratégico'} />
                 </div>
