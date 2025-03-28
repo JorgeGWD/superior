1bc35aaaff4de744784cbfdec0c2f6df075f0dc5
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-28 15:37:50 -0500

    Configure url conditional for redirect to home with the logo

--- src/components/commons/navbar/navbar.jsx
+++ src/components/commons/navbar/navbar.jsx
@@ -3,13 +3,16 @@ import LogoSuperior from '../../../assets/images/logo-superior.svg'
 import IconMenu from '../../../assets/images/icon-menu.svg'
 import IconClose from '../../../assets/images/icon-close.svg'
 import Button from '../button/button'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { useLocation , Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const location = useLocation();
+    const isContactPage = location.pathname.endsWith('/contacto');
+
     return (
         <nav>
             <div className="container navbar__content">
-                <a href='#home'><img src={LogoSuperior} alt="Logo Superior" /></a>
+                <a href={isContactPage ? "/" : "#home"}><img src={LogoSuperior} alt="Logo Superior" /></a>
                 <input type="checkbox" id="check" />
                 <label htmlFor="check" className="navbar__content-checkbtn">
                     <img src={IconMenu} alt="Menu" className='open-menu' />
@@ -20,7 +23,7 @@ const Navbar = () => {
                     <a href="#about">Sobre Nosotros</a>
                     <a href="#projects">Proyectos</a>
                     <a href="#our-facilities">Nuestras Plantas</a>
-                    <Link to={'/contact'}><Button className={'button__primary'} text={'Contacto'} /></Link>
+                    <Link to={'/contacto'}><Button className={'button__primary'} text={'Contacto'} /></Link>
                 </div>
             </div>
         </nav>
