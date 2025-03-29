efb6146108eeb1f015c93cc11b2c18c6fa635ba8
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-29 12:51:37 -0500

    Refactor components

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,19 +1,19 @@
-import About from '../about/about';
-import Projects from '../projects/projects';
-import Services from '../services/services';
-import Slider from '../slider/slider';
-import TopHome from '../top-home/top-home';
+import TopHome from './top-home/top-home'
+import HomeServices from './home-services/home-services'
+import HomeAbout from './home-about/home-about'
+import HomeProjects from './home-projects/home-projects'
+import Slider from '../slider/slider'
 
 const Home = () => {
     return (
         <>
             <TopHome />
-            <Services />
-            <About />
-            <Projects />
+            <HomeServices />
+            <HomeAbout />
+            <HomeProjects />
             <Slider />
         </>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
