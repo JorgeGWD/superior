f6cd73f6e9f71139c4f7c5871c77924017e6df79
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-28 21:28:10 -0500

    Change pathname and adjust href variables

--- src/components/commons/navbar/navbar.jsx
+++ src/components/commons/navbar/navbar.jsx
@@ -7,22 +7,22 @@ import { useLocation , Link } from 'react-router-dom'
 
 const Navbar = () => {
     const location = useLocation();
-    const isContactPage = location.pathname.endsWith('/contacto');
+    const isHomePage = location.pathname === '/';
 
     return (
         <nav>
             <div className="container navbar__content">
-                <a href={isContactPage ? "/" : "#home"}><img src={LogoSuperior} alt="Logo Superior" /></a>
+                <a href={isHomePage ? "#home" : "/"}><img src={LogoSuperior} alt="Logo Superior" /></a>
                 <input type="checkbox" id="check" />
                 <label htmlFor="check" className="navbar__content-checkbtn">
                     <img src={IconMenu} alt="Menu" className='open-menu' />
                     <img src={IconClose} alt="Close" className='close-menu' />
                 </label>
                 <div className="navbar__content-menu">
-                    <a href="#services">Servicios</a>
-                    <a href="#about">Sobre Nosotros</a>
-                    <a href="#projects">Proyectos</a>
-                    <a href="#our-facilities">Nuestras Plantas</a>
+                    <a href={isHomePage ? "#services" : "/services"}>Servicios</a>
+                    <a href={isHomePage ? "#about" : "/about"}>Sobre Nosotros</a>
+                    <a href={isHomePage ? "#projects" : "/projects"}>Proyectos</a>
+                    <a href={isHomePage ? "#our-facilities" : "/our-facilities"}>Nuestras Plantas</a>
                     <Link to={'/contacto'}><Button className={'button__primary'} text={'Contacto'} /></Link>
                 </div>
             </div>
