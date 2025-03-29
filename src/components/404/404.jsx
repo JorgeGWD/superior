2f87f534a4672ccd692d03dfaaa821abedec3594
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-29 13:28:22 -0500

    Set 404 header background image

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,11 +1,12 @@
 import Title from '../commons/title/title';
 import MaquinariaAmarilla from '../../assets/images/maquinaria-amarilla.gif'
+import BackgroundIMG from '../../assets/images/proyectos.jpg'
 import './404.css'
 
 const NotFound = () => {
     return (
         <div className='not-found'>
-            <div className="header not-faund__header">
+            <div className="header not-faund__header" style={{ backgroundImage: `url(${BackgroundIMG})` }}>
                 <div className="container not-faund__header-content">
                     <Title text={'404 - Page Not Found'} className={'title__primary'} />
                 </div>
