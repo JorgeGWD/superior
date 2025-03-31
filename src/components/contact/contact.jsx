b0a514e9e3f3ce478fcc37d27cb04a8e71b7cbbe
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-03-31 17:10:36 -0500

    Adjust contact component with template component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,34 +1,27 @@
-import Button from '../commons/button/button';
-import Title from '../commons/title/title';
+import SectionTemplate from '../commons/section-template/section-template'
+import Button from '../commons/button/button'
 import BackgroundIMG from '../../assets/images/planta-de-trituracion-de-arena.jpg'
 import './contact.css'
 
 const Contact = () => {
     return (
-        <div className='contact'>
-            <div className="header contact__header" style={{ backgroundImage: `url(${BackgroundIMG})` }}>
-                <div className="container contact__header-content">
-                    <Title text={'Contáctanos'} className={'title__primary'} />
+        <SectionTemplate className='contact'  text='Contactanos' BackgroundIMG={BackgroundIMG}>
+            <form className='contact__content-form'>
+                <div>
+                    <label htmlFor="name">Nombre:</label>
+                    <input type="text" id="name" name="name" />
                 </div>
-            </div>
-            <section className='container'>
-                <form className='contact__content'>
-                    <div>
-                        <label htmlFor="name">Nombre:</label>
-                        <input type="text" id="name" name="name" />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" />
-                    </div>
-                    <div>
-                        <label htmlFor="message">Mensaje:</label>
-                        <textarea id="message" name="message"></textarea>
-                    </div>
-                    <Button className={'button__secondary'} text={'Submit'} type={'submit'} />
-                </form>
-            </section>
-        </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Mensaje:</label>
+                    <textarea id="message" name="message"></textarea>
+                </div>
+                <Button className={'button__secondary'} text={'Enviar'} type={'submit'} />
+            </form>
+        </SectionTemplate>
     );
 };
 
