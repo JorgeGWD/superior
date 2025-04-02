93758946d67b60d5f722c885aa8810ff4b932a19
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-01 22:50:01 -0500

    Fix images size and add aspec ratio

--- src/components/commons/imgframe/imgframe.jsx
+++ src/components/commons/imgframe/imgframe.jsx
@@ -4,7 +4,7 @@ const ImgFrame = ({ src, alt, text }) => {
     return (
         <div className="img-frame">
             <div className="img-frame__content">
-                <img src={src} alt={alt} />
+                <img width='500' height='280' src={src} alt={alt} />
                 <p>{text}</p>
             </div>
             <h3>{alt}</h3>
