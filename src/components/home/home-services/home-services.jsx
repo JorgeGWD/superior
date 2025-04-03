c83503dd2deae695745d32e296ee1dbd00eb3b57
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-03 15:18:09 -0500

    Adjust component to import whit selected services

--- src/components/home/home-services/home-services.jsx
+++ src/components/home/home-services/home-services.jsx
@@ -1,28 +1,38 @@
-import ImgFrame from '../../commons/imgframe/imgframe'
-import Title from '../../commons/title/title'
+import ImgFrame from '../../commons/imgframe/imgframe';
+import Title from '../../commons/title/title';
 import Service1 from '../../../assets/images/servicio-movimiento-de-tierra-header.jpg'
 import Service2 from '../../../assets/images/servicio-agregados-header.jpg'
 import Service3 from '../../../assets/images/servicio-concreto-header.jpg'
 import Service4 from '../../../assets/images/excavaciones-profundas.jpg'
 import Service5 from '../../../assets/images/certificado-rcd.jpg'
 import Service6 from '../../../assets/images/servicio-alquiler-de-equipos-header.jpg'
-import './home-services.css'
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import './home-services.css';
 
-const HomeServices = () => {
+const services = [
+    { id: 'movimiento-de-tierra', img: Service1, alt: 'Movimiento de tierra' },
+    { id: 'agregados', img: Service2, alt: 'Agregados' },
+    { id: 'concretos', img: Service3, alt: 'Concretos' },
+    { id: 'excavaciones-profundas', img: Service4, alt: 'Excavaciones profundas' },
+    { id: 'certificado-rcd', img: Service5, alt: 'Certificado RCD' },
+    { id: 'alquiler-de-maquinaria-amarilla', img: Service6, alt: 'Alquiler de Maquinaria Amarilla' }
+];
+
+const HomeServices = ({ visibleServices = [], text }) => {
     return (
         <section className='container home-services' id='inicio-servicios'>
-            <Title className={'title__content-line'} text={'¿Qué Hacemos?'} />
+            <Title className={'title__content-line'} text={text} />
             <div className="home-services__content">
-                <Link to={'/servicios/movimiento-de-tierra'}><ImgFrame src={Service1} alt={'Movimiento de tierra'} text={'Ver más'} /></Link>
-                <Link to={'/servicios/agregados'}><ImgFrame src={Service2} alt={'Agregados'} text={'Ver más'} /></Link>
-                <Link to={'/servicios/concretos'}><ImgFrame src={Service3} alt={'Concretos'} text={'Ver más'} /></Link>
-                <Link to={'/servicios/excavaciones-profundas'}><ImgFrame src={Service4} alt={'Excavaciones profundas'} text={'Ver más'} /></Link>
-                <Link to={'/servicios/certificado-rcd'}><ImgFrame src={Service5} alt={'Certificado RCD'} text={'Ver más'} /></Link>
-                <Link to={'/servicios/alquiler-de-maquinaria-amarilla'}><ImgFrame src={Service6} alt={'Alquiler de Maquinaria Amarilla'} text={'Ver más'} /></Link>
+                {services
+                    .filter(service => visibleServices.includes(service.id))
+                    .map(service => (
+                        <Link key={service.id} to={`/servicios/${service.id}`}>
+                            <ImgFrame src={service.img} alt={service.alt} text={'Ver más'} />
+                        </Link>
+                    ))}
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default HomeServices
+export default HomeServices;
