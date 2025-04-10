56e82f177a0136402d7bd6a6a974cf29b4b97880
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-10 15:45:13 -0500

    Set iso logo in service earthmoving component

--- src/components/services/service-earthmoving/service-earthmoving.jsx
+++ src/components/services/service-earthmoving/service-earthmoving.jsx
@@ -6,6 +6,7 @@ import ImageGallery2 from '../../../assets/images/servicio-movimiento-de-tierra-
 import ImageGallery3 from '../../../assets/images/servicio-movimiento-de-tierra-gallery-3.jpg'
 import ImageRibbon from '../../commons/image-ribbon/image-ribbon'
 import HomeServices from '../../home/home-services/home-services'
+import IsoLogo from '../../commons/iso-logo/iso-logo'
 import './service-earthmoving.css'
 
 const ServiceEarthmoving = () => {
@@ -21,6 +22,7 @@ const ServiceEarthmoving = () => {
                 <div className="section-template__content-text service-earthmoving__content-text">
                     <h2>Somos especialistas en la ejecución de movimientos de tierra, excavaciones masivas, y obras complementarias.</h2>
                     <p>A la hora de planificar una construcción y antes de empezar cualquier tipo de obra, es necesario realizar una serie de trabajos para preparar el terreno lo cual nos permite empezar a realizar la cimentación y posterior edificación, contamos con una amplia experiencia desarrollando proyectos para diferentes empresas industriales de la región</p>
+                    <IsoLogo />
                 </div>
                 <img src={EarthMovingIMG} alt="Movimiento de tierra" />
             </SectionTemplate>
