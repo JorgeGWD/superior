e9d84ddf48ce2f566e1989f3351cdb450ee5291e
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-10 20:52:36 -0500

    Change a to react router Link and prevent onclik inside services

--- src/components/commons/navbar/navbar.jsx
+++ src/components/commons/navbar/navbar.jsx
@@ -3,44 +3,59 @@ import LogoSuperior from '../../../assets/images/logo-superior.svg'
 import IconMenu from '../../../assets/images/icon-menu.svg'
 import IconClose from '../../../assets/images/icon-close.svg'
 import Button from '../button/button'
-import { useLocation , Link } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 
 const Navbar = () => {
     const location = useLocation();
-    const isHomePage = location.pathname === '/';
+    const isHomePage = location.pathname === '/'
 
     return (
         <nav>
             <div className="container navbar__content">
-                <a href={isHomePage ? "#inicio" : "/"}><img src={LogoSuperior} alt="Logo Superior" /></a>
+                <Link to={isHomePage ? "#inicio" : "/"}><img src={LogoSuperior} alt="Logo Superior" /></Link>
+
                 <input type="checkbox" id="check" />
                 <label htmlFor="check" className="navbar__content-checkbtn">
                     <img src={IconMenu} alt="Menu" className='open-menu' />
                     <img src={IconClose} alt="Close" className='close-menu' />
                 </label>
+
                 <div className="navbar__content-menu">
                     <div className="navbar__content-menu-dropdown">
                         <div className="navbar__content-menu-dropdown-item">
-                            <a href={isHomePage ? "#inicio-servicios" : ""}>Servicios</a>
-                            <svg viewBox="0 0 21 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.5 2L3.5 17L18.5 32" stroke-width="4" stroke-linecap="round"/></svg>
+                            {isHomePage ? (
+                                <a href="#inicio-servicios">Servicios</a>
+                                ) : (
+                                <Link onClick={(e) => e.preventDefault()}>Servicios</Link>
+                            )}
+                            <svg viewBox="0 0 21 34" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.5 2L3.5 17L18.5 32" strokeWidth="4" strokeLinecap="round"/></svg>
                         </div>
                         <div className="navbar__content-menu-dropdown-content">
-                            <a href="/servicios/movimiento-de-tierra">Movimiento de tierra</a>
-                            <a href="/servicios/agregados">Agregados</a>
-                            <a href="/servicios/concretos">Concretos</a>
-                            <a href="/servicios/excavaciones-profundas">Excavaciones profundas</a>
-                            <a href="/servicios/certificado-rcd">Certificado RCD</a>
-                            <a href="/servicios/alquiler-de-maquinaria-amarilla">Alquiler de maquinaria  amarilla</a>
+                            <Link to="/servicios/movimiento-de-tierra">Movimiento de tierra</Link>
+                            <Link to="/servicios/agregados">Agregados</Link>
+                            <Link to="/servicios/concretos">Concretos</Link>
+                            <Link to="/servicios/excavaciones-profundas">Excavaciones profundas</Link>
+                            <Link to="/servicios/certificado-rcd">Certificado RCD</Link>
+                            <Link to="/servicios/alquiler-de-maquinaria-amarilla">Alquiler de maquinaria amarilla</Link>
                         </div>
                     </div>
-                    <a href={isHomePage ? "#inicio-sobre-nosotros" : "/sobre-nosotros"}>Sobre Nosotros</a>
-                    {/*<a href={isHomePage ? "#inicio-proyectos" : "/proyectos"}>Proyectos</a>*/}
-                    {/*<a href={isHomePage ? "#inicio-nuestras-plantas" : "/nuestras-plantas"}>Nuestras Plantas</a>*/}
-                    <Link to={'/contacto'}><Button className={'button__primary'} text={'Contacto'} /></Link>
+
+                    {isHomePage ? (
+                        <a href="#inicio-sobre-nosotros">Sobre Nosotros</a>
+                    ) : (
+                        <Link to="/sobre-nosotros">Sobre Nosotros</Link>
+                    )}
+
+                    {/* <a href={isHomePage ? "#inicio-proyectos" : "/proyectos"}>Proyectos</a> */}
+                    {/* <a href={isHomePage ? "#inicio-nuestras-plantas" : "/nuestras-plantas"}>Nuestras Plantas</a> */}
+
+                    <Link to="/contacto">
+                        <Button className={'button__primary'} text={'Contacto'} />
+                    </Link>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar
