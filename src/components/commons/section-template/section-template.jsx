aecbd9b2db196540018990d4e3c9321b72a9c1ee
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-21 16:44:18 -0500

    Add a button in header

--- src/components/commons/section-template/section-template.jsx
+++ src/components/commons/section-template/section-template.jsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router'
+import Button from '../button/button'
 import Title from '../title/title'
 import './section-template.css'
 
-const SectionTemplate = ({ className, text, children, BackgroundIMG }) => {
+const SectionTemplate = ({ className, text, children, BackgroundIMG, showButton = false }) => {
     return (
         <div className={className}>
             <div className={`header ${className}__header`} style={{ backgroundImage: `url(${BackgroundIMG})` }} >
-                <div className={`container ${className}__header-content`}>
+                <div className={`container ${className}__header-content section-template__header-content`}>
                     <Title text={text} className='title__primary' />
+                    {showButton && (
+                        <Link to="/contacto">
+                            <Button className={'button__secondary'} text={'Pide una cotización'} />
+                        </Link>
+                    )}
                 </div>
             </div>
             <section className={`container section-template ${className}__content`}>
