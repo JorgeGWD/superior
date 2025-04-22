4826eea4eb4f6e8d993d24a8cf352bb3b18c5f23
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-04-22 15:32:32 -0500

    Save and configure service certified rcd image

--- src/components/services/service-certified-rcd/service-certified-rcd.jsx
+++ src/components/services/service-certified-rcd/service-certified-rcd.jsx
@@ -1,12 +1,14 @@
 import IsoLogo from '../../commons/iso-logo/iso-logo'
 import SectionTemplate from '../../commons/section-template/section-template'
+import BackgroundIMG from '../../../assets/images/servicio-certificado-rcd-header.jpg'
+import CetificateRCD from '../../../assets/images/servicio-certificado-rcd-featured.jpg'
 import HomeServices from '../../home/home-services/home-services'
 import './service-certified-rcd.css'
 
 const ServiceCertifiedRcd = () => {
     return (
         <>
-            <SectionTemplate className='service-certified-rcd' text='Certificado RCD' showButton={true}>
+            <SectionTemplate className='service-certified-rcd' text='Certificado RCD' BackgroundIMG={BackgroundIMG} showButton={true}>
                 <div className="section-template__content-text">
                     <h2>Contamos con certificado de aprovechamiento de residuos RCD para su tratamiento y clasificación asegurando un aprovechamiento óptimo de los mismos siendo gestores ambientales sostenibles.</h2>
                     <h2>RCD (Residuos de Concretos)</h2>
@@ -15,6 +17,7 @@ const ServiceCertifiedRcd = () => {
                     <p>Provenientes de excavaciones de obras y limpieza de terrenos u otras actividades similares.</p>
                     <IsoLogo />
                 </div>
+                <img src={CetificateRCD} alt="Certificado RCD" />
             </SectionTemplate>
             <HomeServices className='title__section-content' text='Otros Servicios' visibleServices={['movimiento-de-tierra', 'agregados', 'concretos', 'excavaciones-profundas', 'alquiler-de-maquinaria-amarilla']} />
         </>
