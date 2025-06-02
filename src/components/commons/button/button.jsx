9d328aefef8f015162b87605d7e805f04af39f5a
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-06-01 20:18:02 -0500

    Add onClick parameter

--- src/components/commons/button/button.jsx
+++ src/components/commons/button/button.jsx
@@ -1,8 +1,8 @@
 import './button.css'
 
-const Button = ({ text, className, type }) => {
+const Button = ({ text, className, type, onClick }) => {
     return (
-        <button className={`button ${className}`} type={type}>
+        <button className={`button ${className}`} type={type} onClick={onClick}>
             {text}
         </button>
     )
